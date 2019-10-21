d8bc02b53ae9d1b8e475be39885806d98fba285c
ViodDevelopment <viodDevelopment@gmail.com>
2019-10-21 12:49:17 +0200

    Nosequeva

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    public GameObject m_Hornero;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            
+        }
+        else
+        {
+            DestroyImmediate(this);
+        }
+
+
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameManager();
+            }
+            return instance;
+        }
+    }
+
+    public void Update()
+    {
+        if(m_Hornero.GetComponent<Animator>().GetBool("Hablando")&&!m_Hornero.GetComponent<AudioSource>().isPlaying)
+        {
+            StopHornero();
+        }
+    }
+
+    public void HorneroTrigger()
+    {
+        print("hola");
+        m_Hornero.GetComponent<Animator>().SetBool("Hablando", true);
+        m_Hornero.GetComponent<AudioSource>().Play();
+    }
+
+    public void StopHornero()
+    {
+        m_Hornero.GetComponent<Animator>().SetBool("Hablando", false);
+    }
+}
