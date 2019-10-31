8120fb53b7f1985718384f37e7f80b513d4443cd
Sergio Rodriguez <sergio1811@hotmail.es>
2019-10-31 12:52:37 +0100

    Todas las animaciones + creditos

---
+++ Assets/ChargeMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChargeMenu : MonoBehaviour
+{
+    public AnimationEvent CallScene;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+     void ChargeScene()
+    {
+         SceneManager.LoadScene(1);
+    }
+}
