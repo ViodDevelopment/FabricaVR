407e6758d7520207d2c286e479f93340f5ba7e6a
SerTor2 <sertor27@gmail.com>
2019-11-05 17:17:06 +0100

    fade in

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    public Image image;
+    public float speedToClear = 0.4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.color = image.color + new Color(0, 0, 0, 1);
+        image.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        image.color = image.color - new Color(0, 0, 0, speedToClear * Time.deltaTime);
+        if (image.color.a <= 0.05)
+        {
+            image.gameObject.SetActive(false);
+            gameObject.GetComponent<FadeIn>().enabled = false;
+        }
+
+    }
+}
