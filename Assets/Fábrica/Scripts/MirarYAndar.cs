0891b4035cdaa79d078c27b48c270ab5462ad700
ViodDevelopment <46675059+ViodDevelopment@users.noreply.github.com>
2019-11-07 12:05:03 +0100

    tag cuboobjetivo

---
+++ Assets/Fábrica/Scripts/MirarYAndar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirarYAndar : MonoBehaviour
+{
+    public LayerMask layer;
+    public GoToCube goToCube;
+    public Camera camera;
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit rayhit;
+        if(Physics.Raycast(camera.transform.position, camera.transform.forward,out rayhit, 200, layer))
+        {
+
+        }
+    }
+}
