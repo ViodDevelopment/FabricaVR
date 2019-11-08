44166a7285afa3f32f95ba16150090fbf757db18
Sergio Rodriguez <sergio1811@hotmail.es>
2019-11-08 12:57:01 +0100

    Animaciones

--- Assets/Fábrica/Scripts/ActivateCubes.cs
+++ Assets/Fábrica/Scripts/ActivateCubes.cs
@@ -7,6 +7,10 @@ public class ActivateCubes : MonoBehaviour
     public GameObject[] m_CubesList;
     private GameObject m_Player;
     private int l_Cube = 0;
+    public GameObject CuboAceite;
+    public GameObject CuboPalanca;
+    public Animation AceiteAnimation;
+    public Animation PalancaAnimation;
 
     public int l_ActionsCubes;
 
@@ -19,7 +23,7 @@ public class ActivateCubes : MonoBehaviour
     {
         if (l_Cube < m_CubesList.Length && m_CubesList[l_Cube] != null)
         {
-            if (Vector3.Distance(m_Player.transform.position, m_CubesList[l_Cube].transform.position) < 0.9f && m_CubesList[l_Cube].active)
+            if (Vector3.Distance(m_Player.transform.position, m_CubesList[l_Cube].transform.position) < 0.9f && m_CubesList[l_Cube].activeSelf)
             {
                 Destroy(m_CubesList[l_Cube]);
                 l_ActionsCubes++;
@@ -28,7 +32,24 @@ public class ActivateCubes : MonoBehaviour
                 {
                     if(m_Player.GetComponent<PlayerBehaviour>() != null)
                         m_CubesList[l_Cube].SetActive(true);
+                    else
+                    {
+                        if(CuboAceite ==null)
+                        {
+                            AceiteAnimation.Play();
+                        }
+                    }
                 }
+            }
+        }
+        if (m_Player.GetComponent<PlayerBehabioursPlanta2>() != null) { 
+            if (CuboAceite == null )
+            {              
+                AceiteAnimation.Play();
+            }
+            if (CuboPalanca == null )
+            {              
+                PalancaAnimation.Play();
             }
         }
     }
